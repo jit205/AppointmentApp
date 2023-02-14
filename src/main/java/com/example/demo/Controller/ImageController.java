2787ad306f1510f799e533d2b71655480a7c84a1
jit205 <jittamsakhia02@gmail.com>
2023-02-14 21:09:08 +0530

    image upload

---
+++ src/main/java/com/example/demo/Controller/ImageController.java
@@ -0,0 +1,57 @@
+package com.example.demo.Controller;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.Model.ImageData;
+import com.example.demo.Services.ImageService;
+
+import jakarta.annotation.Resource;
+
+
+@RestController
+@RequestMapping("/images")
+public class ImageController {
+	
+
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    
+    @PostMapping()
+    public String uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+    	ImageData image = new ImageData();
+        image.setName(file.getOriginalFilename());
+        image.setContentType(file.getContentType());
+        image.setData(file.getBytes());
+        mongoTemplate.save(image);
+        return "Image uploaded successfully";
+    }
+    @Autowired
+    private ImageService imageService;
+
+    
+
+    @GetMapping("/image/{id}")
+    public ResponseEntity<Resource> getImage(@PathVariable String id) throws IOException {
+        Resource imageResource = imageService.getImageById(id);
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .body(imageResource);
+    }
+
+    
+    
+}
---
+++ src/main/java/com/example/demo/Model/ImageData.java
@@ -0,0 +1,28 @@
+package com.example.demo.Model;
+
+public class ImageData {
+	
+    private String name;
+    private String contentType;
+    private byte[] data;
+
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContentType() {
+		return contentType;
+	}
+	public void setContentType(String contentType) {
+		this.contentType = contentType;
+	}
+	public byte[] getData() {
+		return data;
+	}
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+	
+}
